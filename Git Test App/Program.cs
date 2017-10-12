6de3c2d4b9533a57526b154ca5d596ca4fca7b0e
Antonescu, Dan <dan.antonescu@swarco.com>
2017-10-12 17:51:02 +0300

    dev1.03

--- Git Test App/Program.cs
+++ Git Test App/Program.cs
@@ -19,7 +19,7 @@ namespace Git_Test_App
         static void FeatureD()
         {
             Console.WriteLine("Feature D.03");
-            Console.WriteLine("dev1.02");
+            Console.WriteLine("dev1.03");
         }
     }
 }
